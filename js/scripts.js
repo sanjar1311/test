0ba314308fda482cead67a3157785e9fb3848871
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-27 22:05:04 -0600

    make idel modal

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,53 @@ var createElement = function (tagName, className, text) {
 
   return element;
 };
+
+
+var elModalOpenBtn = $_('.js-modal-btn');
+var elModal = $_('.modal');
+var elModalCloseBtn = $_('.js-modal-close');
+
+
+function openModal() {
+  elModal.classList.add('modal--open');
+}
+
+function modalClose() {
+  elModal.classList.remove('modal--open');
+}
+
+function docClick(e) {
+  if(e.target.matches('.modal')) {
+    modalClose();
+    elModalOpenBtn.addEventListener('click', modalOpenBtnClick);
+    elModalCloseBtn.removeEventListener('click', modalCloseBtnClick);
+    elModal.removeEventListener('click', docClick);
+  }
+}
+
+function modalOpenBtnClick() {
+  openModal();
+  elModalCloseBtn.addEventListener('click', modalCloseBtnClick);
+  elModal.addEventListener('click', docClick);
+  elModalOpenBtn.removeEventListener('click', modalOpenBtnClick);
+}
+
+function modalCloseBtnClick() {
+  modalClose();
+  elModalOpenBtn.addEventListener('click', modalOpenBtnClick);
+  elModalCloseBtn.removeEventListener('click', modalCloseBtnClick);
+  elModal.removeEventListener('click', docClick);
+}
+
+function escapePress(e) {
+  if(e.key === "Escape") {
+    modalClose();
+    elModalOpenBtn.addEventListener('click', modalOpenBtnClick);
+    elModalCloseBtn.removeEventListener('click', modalCloseBtnClick);
+    elModal.removeEventListener('click', docClick);
+  }
+}
+
+elModalOpenBtn.addEventListener('click', modalOpenBtnClick);
+
+document.addEventListener('keyup', escapePress);
